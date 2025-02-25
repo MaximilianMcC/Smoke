2419bc527f0737b6418dde3c0f549ac82b3dcfc8
MaximilianMcC <max@maximilian.co.nz>
2025-02-26 01:08:20 +1300

    Raylib setup

---
+++ Engine/src/Program.cs
@@ -0,0 +1,37 @@
+﻿using Raylib_cs;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		// Setup raylib
+		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
+		Raylib.SetConfigFlags(ConfigFlags.ResizableWindow | ConfigFlags.AlwaysRunWindow);
+		Raylib.InitWindow(10, 10, "Marl Engine");
+		Raylib.SetExitKey(KeyboardKey.Null);
+
+		// Set the window to be half the size of the monitor rn
+		// And also put it in the centre of the screen
+		// TODO: Maybe don't scope stuff like this
+		{
+			Raylib.SetWindowSize(
+				Raylib.GetMonitorWidth(Raylib.GetCurrentMonitor()) / 2,
+				Raylib.GetMonitorHeight(Raylib.GetCurrentMonitor()) / 2
+			);
+			Raylib.SetWindowPosition(
+				(Raylib.GetMonitorWidth(Raylib.GetCurrentMonitor()) / 2) / 2,
+				(Raylib.GetMonitorHeight(Raylib.GetCurrentMonitor()) / 2) / 2
+			);
+		}
+
+		// Main program loop
+		while (Raylib.WindowShouldClose() == false)
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.Magenta);
+			Raylib.EndDrawing();
+		}
+
+		Raylib.CloseWindow();
+	}
+}
