37117a7aa3163c772e71d8f72b73c50106e8d358
MaximilianMcC <max@maximilian.co.nz>
2025-05-24 15:34:20 +1200

    Sprites can be looped + "events" are shown

--- Library/src/Components/Sprite.cs
+++ Library/src/Components/Sprite.cs
@@ -7,33 +7,79 @@ public class Sprite : UpdatableComponent
 {
 	public Texture2D Texture { get; private set; }
 	public List<string> Frames = [];
+
 	public int FrameIndex;
+	public bool LoopAnimation = true;
+	public bool JustSwitchedToNextFrame { get; private set; }
+	public bool JustDidAFullLoop { get; private set; }
 
 	public float Fps;
 	private float elapsedTime;
 
-	public void AddFrames(params string[] textureKeys) => Frames.AddRange(textureKeys);
+	public void SetFrames(params string[] textureKeys)
+	{
+		// Make sure they're not trolling
+		// TODO: Maybe don't do this
+		if (textureKeys.Length == 0) return;
+
+		// Add the frames to the frames list
+		// then start at the first frame
+		//? -1 since the index is increased in the method (0)
+		Frames.AddRange(textureKeys);
+		MoveToNextFrame(-1);
+	}
 
 	public override void Update()
 	{
 		// Check for if we have anything to render
 		if (Frames.Count == 0) return;
 
+		// Check for if we need to reset the state status'
+		if (JustSwitchedToNextFrame) JustSwitchedToNextFrame = false;
+		if (JustDidAFullLoop) JustDidAFullLoop = false;
+
 		// Update the frame timer
 		elapsedTime += DeltaTime;
 
 		// Check for if we need to switch frame
-		if (elapsedTime > 1 / Fps)
+		//? 1/fps makes it per second I think idk i js know it works
+		if (elapsedTime > (1 / Fps))
 		{
 			// Reset the timer
 			elapsedTime = 0;
 
-			// Move onto the next frame
-			FrameIndex++;
-			FrameIndex %= Frames.Count;
-			Texture = Textures[Frames[FrameIndex]];
-
-			// 
+			// Update the texture
+			MoveToNextFrame();
 		}
+	}
+
+	public void MoveToNextFrame()
+	{
+		// Update the index
+		FrameIndex++;
+		JustSwitchedToNextFrame = true;
+
+		// Check for if we did a full loop
+		if (FrameIndex >= Frames.Count)
+		{
+			FrameIndex = 0;
+			JustDidAFullLoop = true;
+
+			// Check for if we don't want to loop again (suicide)
+			if (LoopAnimation == false)
+			{
+				Enabled = false;
+				return;
+			}
+		}
+
+		// Update the current texture
+		Texture = Textures[Frames[FrameIndex]];
+	}
+
+	public void MoveToNextFrame(int index)
+	{
+		FrameIndex = index;
+		MoveToNextFrame();
 	}
 }
