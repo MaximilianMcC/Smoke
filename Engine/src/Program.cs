7f65611fe178cdccf445e5dff4b1b5fdf131951e
MaximilianMcC <max@maximilian.co.nz>
2025-06-13 08:35:56 +1200

    some error things work again

--- Engine/src/Program.cs
+++ Engine/src/Program.cs
@@ -10,21 +10,29 @@ class Program
 {
 	public static void Main(string[] args)
 	{
+		// TODO: Remove this
+		//! debug
+		Console.Clear();
+
 		// Check for if we wanna use it as a cli tool
 		if (args.Length > 0)
 		{
 			// Register/make all the commands
 			Command publishCommand = new Command(
-				"Publish", "build and compile a project to be shipped",
+				"publish", "Build and compile a project to be shipped",
 				new Argument("game.json path", "Path of the game.json file", "./game.json"),
 				new Argument("game.dll path", "Path of the compiled game.dll file", "./game.dll"),
 				new Argument("output path", "published output directory", "D:/temp/games", true)
 			);
 
 			// Check for what command was ran
-			if (ArgumentParser.CommandRan(publishCommand, args))
+			// TODO: Don't use var
+			Dictionary<string, string> ranCommand;
+			if (ArgumentParser.CommandRan(publishCommand, args, out ranCommand))
 			{
-
+				Console.WriteLine(ranCommand["game.json path"]);
+				Console.WriteLine(ranCommand["game.dll path"]);
+				Console.WriteLine(ranCommand["output path"]);
 			}
 		}
 	}
--- Engine/src/cli/ArgumentParser.cs
+++ Engine/src/cli/ArgumentParser.cs
@@ -1,16 +1,37 @@
 static class ArgumentParser
 {
-	public static bool CommandRan(Command command, string[] args)
+	public static List<Command> AllCommands = [];
+	public static Command HelpCommand = new Command("help", "Shows all commands");
+
+	public static bool CommandRan(Command command, string[] args, out Dictionary<string, string> output)
 	{
-		// Check for if they want help on the command
-		if (args[0].ToLower() == "help")
+		// Extract the actual command
+		string ranCommandName = args[0].ToLower().Trim();
+
+		// Check for if they ran the help command
+		if (ranCommandName == HelpCommand.Name)
 		{
-			command.PrintFullUsage();
+			// Show the help command
+			PrintHelp();
+
+			output = null;
 			return false;
 		}
 
-		// Check for if the command name is the same
-			if (command.Name != args[0].ToLower()) return false;
+		// Check for if they supplied the correct name
+		// (actually ran the command we're after)
+		if (command.Name != ranCommandName)
+		{
+			// Check for if there are ANY commands with the supplied name
+			if (AllCommands.Where(command => command.Name == ranCommandName).Count() == 0)
+			{
+				Console.WriteLine($"The command '{ranCommandName}' wasn't found (check spelling idk)");
+				Console.WriteLine($"You can also type 'help' for a list of commands");
+			}
+
+			output = null;
+			return false;
+		}
 
 		// Check for if the correct amount of arguments are present
 		if ((args.Length < command.MinLength) || (args.Length > command.MaxLength))
@@ -18,14 +39,29 @@ static class ArgumentParser
 			// TODO: Add colors and icons and stuff
 			Console.Error.WriteLine("Invalid argument count!");
 			Console.Error.WriteLine($"Type 'smoke {command.Name} help' for advanced help");
+			Console.WriteLine();
 			command.PrintUsage();
 		}
 
-		int index = 0;
-		return true;
+		// return true;
+		output = null;
+		return false;
+	}
+
+	public static void PrintHelp()
+	{
+		Console.WriteLine("Full command list:");
+		foreach (Command command in AllCommands)
+		{
+			Console.WriteLine($"{command.Name}\t\t{command.Description}");
+		}
+		Console.WriteLine();
+		Console.WriteLine("For help on a specific command, type '<command name> help'");
+		Console.WriteLine($"example: '{AllCommands[1].Name} help'");
 	}
 }
 
+// TODO: Make it so you can add aliases to commands (multiple names)
 class Command
 {
 	public string Name;
@@ -37,6 +73,11 @@ class Command
 		Name = name;
 		Description = description;
 		Arguments = arguments;
+
+		// Automatically add the command to the
+		// full command list so the help command
+		// and whatnot is automatically generated
+		ArgumentParser.AllCommands.Add(this);
 	}
 
 	//? 1 is added for the command name
@@ -48,13 +89,23 @@ class Command
 		// TODO: Add colors and icons and stuff
 		Console.WriteLine("Example usage:");
 
+		// Show the command template/syntax
 		Console.Write($"{Name} ");
 		foreach (Argument arg in Arguments) Console.Write($"{arg.GetValue()} ");
 		Console.WriteLine();
 
+		// Show an example (including any optional parameters)
 		Console.Write($"{Name} ");
-		foreach (Argument arg in Arguments) Console.Write($"{arg.GetExample()} ");
+		foreach (Argument arg in Arguments) Console.Write($"{arg.ExampleValue} ");
 		Console.WriteLine();
+
+		// Show an example (including only required parameters)
+		if (Arguments.Where(arg => arg.Optional == false).Count() > 1)
+		{
+			Console.Write($"{Name} ");
+			foreach (Argument arg in Arguments.Where(arg => arg.Optional == false)) Console.Write($"{arg.ExampleValue} ");
+			Console.WriteLine();
+		}
 	}
 
 	public void PrintFullUsage()
@@ -64,7 +115,7 @@ class Command
 		Console.WriteLine($"{Description}");
 		Console.WriteLine();
 
-		PrintFullUsage();
+		PrintUsage();
 		Console.WriteLine();
 
 		foreach (Argument arg in Arguments)
@@ -75,7 +126,7 @@ class Command
 			Console.WriteLine();
 			Console.WriteLine($"Example: {arg.ExampleValue}");
 			Console.WriteLine();
-			
+
 			if (arg.Optional) Console.WriteLine("This is an optional argument.");
 			if (arg.CaseSensitive) Console.WriteLine("This is a case-sensitive optional argument.");
 			Console.WriteLine();
@@ -109,14 +160,9 @@ struct Argument
 	}
 
 	// TODO: Use a property
-	public string GetValue() => GetValue(false);
-	public string GetExample() => GetValue(true);
-
-	private string GetValue(bool example)
+	public string GetValue()
 	{
-		string value = example ? ExampleValue : Name;
-
-		if (Optional) return $"[{value}]";
-		return $"<{value}>";
+		if (Optional) return $"[{Name}]";
+		return $"<{Name}>";
 	}
 }
