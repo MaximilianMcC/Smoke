2fa9dbc4cf400b2ae970777454a7c89944fc9636
MaximilianMcC <max@maximilian.co.nz>
2025-06-13 00:14:52 +1200

    erhm added f idk if that was needed but it feels safer

--- Library/src/Api/NormalisedFloat.cs
+++ Library/src/Api/NormalisedFloat.cs
@@ -4,7 +4,7 @@ public struct NormalisedFloat
 
 	// Keep the number between 0 and 1
 	// TODO: Make a (-1, 1) version, and a (-1, 0, 1) version
-	public NormalisedFloat(float value) => normalisedValue = Math.Clamp(value, 0, 1);
+	public NormalisedFloat(float value) => normalisedValue = Math.Clamp(value, 0f, 1f);
 
 	// Getter
 	public static implicit operator float(NormalisedFloat value) => value.normalisedValue;
