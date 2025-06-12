6d5ad88c4b5ad8bb86784fea91ab4713a60ed53f
MaximilianMcC <max@maximilian.co.nz>
2025-06-13 00:14:19 +1200

    Added a new type that can only be between 0 and 1

---
+++ Library/src/Api/NormalisedFloat.cs
@@ -0,0 +1,14 @@
+public struct NormalisedFloat
+{
+	private readonly float normalisedValue;
+
+	// Keep the number between 0 and 1
+	// TODO: Make a (-1, 1) version, and a (-1, 0, 1) version
+	public NormalisedFloat(float value) => normalisedValue = Math.Clamp(value, 0, 1);
+
+	// Getter
+	public static implicit operator float(NormalisedFloat value) => value.normalisedValue;
+
+	// Setter/constructor
+	public static implicit operator NormalisedFloat(float value) => new NormalisedFloat(value);
+}
