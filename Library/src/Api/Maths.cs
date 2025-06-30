b982041cbc374161805da3607eada75d0fc56c7f
MaximilianMcC <max@maximilian.co.nz>
2025-06-30 19:40:09 +1200

    sorry i lied

--- Library/src/Api/Maths.cs
+++ Library/src/Api/Maths.cs
@@ -2,11 +2,11 @@ public class Maths
 {
 	public static bool InRange(int variable, int lower, int upper)
 	{
-		return (variable >= upper) && (variable <= lower);
+		return (variable >= lower) && (variable <= upper);
 	}
 
 	public static bool InRange(float variable, float lower, float upper)
 	{
-		return (variable >= upper) && (variable <= lower);
+		return (variable >= lower) && (variable <= upper);
 	}
 }
