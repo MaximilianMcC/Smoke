19969b3bd2b1d3ee4dfe5f2c3a3bd8ae79c34f32
MaximilianMcC <max@maximilian.co.nz>
2025-07-15 08:22:31 +1200

    Restarting. Can just write rn

--- Library/src/Components/TextInput.cs
+++ Library/src/Components/TextInput.cs
@@ -1,24 +1,22 @@
 using System.Numerics;
 using static Smoke.Input;
 using static Smoke.Graphics;
+using static Smoke.Presets;
 
 namespace Smoke;
 
 public class TextInput : RenderableComponent
 {
-	public string Text = "";
-	public int CaretIndex = 0;
-	
-	// TODO: Make a caret class with all this + the index
-	private float caretLerpStart;
-	private float caretLerpEnd;
-	private float caretLerpAmount;
+	public List<string> Lines = [];
+	public string Text => string.Join("\n", Lines);
+
+	private List<Caret> carets = [];
+	private Caret mainCaret => carets[0];
+
+	private Selection selection;
 
 	public float FontSize = 32;
 	public Color SelectionColor = 0x162F58_80;
-
-	private Selection selection;
-	private bool selectingSomething => selection != null;
 
 	public override void Update()
 	{
@@ -33,38 +31,6 @@ public class TextInput : RenderableComponent
 				WriteAfterCursor(input[i].ToString());
 			}
 		}
-
-		// Clipboard
-		if (ShortcutDone(KeyboardKey.LeftControl, KeyboardKey.V)) WriteAfterCursor(ClipboardText);
-		if (ShortcutDone(KeyboardKey.LeftControl, KeyboardKey.C))
-		{
-			// If we've got something selected then use that,
-			// otherwise use the entire line/sentence
-			ClipboardText = selectingSomething ? GetSelectedText() : GetCurrentContent();
-		}
-
-		// Deleting
-		if (KeyPressedAndHeld(KeyboardKey.Backspace)) RemoveBeforeCursor();
-		if (KeyPressedAndHeld(KeyboardKey.Delete)) RemoveAfterCursor();
-
-		// Drastic navigation
-		if (KeyPressedAndHeld(KeyboardKey.Home)) GoToStartOfText();
-		if (KeyPressedAndHeld(KeyboardKey.End)) GoToEndOfText();
-
-		// General navigation
-		if (KeyPressedAndHeld(KeyboardKey.Left))
-		{
-			Deselect();
-			MoveLeft();
-		}
-		if (KeyPressedAndHeld(KeyboardKey.Right))
-		{
-			Deselect();
-			MoveRight();
-		}
-
-		// Selecting
-		if (ShortcutDone(KeyboardKey.LeftControl, KeyboardKey.A)) SelectAll();
 	}
 
 	// Draw everything idk
@@ -73,154 +39,22 @@ public class TextInput : RenderableComponent
 		//!debug
 		Vector2 position = new Vector2(20);
 
-		// Draw the section
-		if (selectingSomething)
-		{
-			// Get the size of the selection
-			Vector2 selectionSize = MeasureText(GetSelectedText(), FontSize);
-
-			// Get the position of the selection
-			Vector2 selectionPosition = position + (MeasureText(GetContentBeforeCaret(), FontSize) * Vector2.UnitX);
-
-			// Draw the selection thingy (behind the text)
-			// TODO: Draw in the front
-			DrawSquare(selectionPosition, selectionSize, SelectionColor);
-		}
-
-		// Draw the text
+		// Draw the text normally ig
 		DrawText(Text, position, Origin.TopLeft, 0f, FontSize, Color.White);
-
-		// Draw the caret
-		// TODO: Animate it
-		// TODO: Add block caret support
-		string textUpUntilCaret = Text.Substring(0, CaretIndex);
-		Vector2 caretPosition = position + (MeasureText(textUpUntilCaret, FontSize) * Vector2.UnitX);
-		DrawSquare(caretPosition, new Vector2(FontSize / 15, FontSize), Color.White);
 	}
 
-	// 'Paste' text where the cursor is
+	// Append text where the cursor is (normal writing)
 	private void WriteAfterCursor(string newText)
 	{
-		Text = Text.Insert(CaretIndex, newText);
-		MoveRight(newText.Length);
-	}
-
-	// Delete selected stuff
-	private void DeleteSelectedStuff()
-	{
-		// Check for if theres anything to delete
-		if (selectingSomething == false) return;
-
-		//! this MIGHT be reused code idk though
-		Text = Text.Remove(selection.StartIndex, selection.EndIndex);
-		CaretIndex -= selection.Length;
-		Deselect();
-	}
-
-	// Remove x letters from the cursors position
-	private void RemoveAfterCursor(int charactersToRemove = 1)
-	{
-		// If we've got selected stuff to
-		// delete then just delete it 
-		DeleteSelectedStuff();
-
-		// Make sure we're within bounds (can't delete nothing)
-		if (CaretIndex < Text.Length)
+		// Loop over all the carets
+		// TODO: Make a caret handler class that does the looping for me
+		foreach (Caret caret in carets)
 		{
-			// Remove the characters after/right of the caret
-			Text = Text.Remove(CaretIndex, charactersToRemove);
+			// Add the text 
+			Lines[caret.Line] = Lines[caret.Line].Insert(caret.Index, newText);
+			caret.MoveForwards(newText.Length);
 		}
 	}
-	private void RemoveBeforeCursor(int charactersToRemove = 1)
-	{
-		// If we've got selected stuff to
-		// delete then just delete it 
-		DeleteSelectedStuff();
-
-		// Make sure we're within bounds (can't delete nothing)
-		if (CaretIndex > 0)
-		{
-			// Remove the characters before/left of the caret
-			Text = Text.Remove(CaretIndex - 1, charactersToRemove);
-			CaretIndex -= charactersToRemove;
-		}
-	}
-
-	// Drastically jump between text
-	private void GoToStartOfText() => CaretIndex = 0;
-	private void GoToEndOfText() => CaretIndex = Text.Length;
-
-	// Jump between sentences
-	private void GoToStartOfSentence() => throw new NotImplementedException();
-	private void GoToEndOfSentence() => throw new NotImplementedException();
-
-	// Move around
-	private void MoveLeft(int charactersToMove = 1)
-	{
-		// Check for if we have somewhere to move to
-		if (!Maths.InRange(CaretIndex, 0, Text.Length)) return;
-		CaretIndex -= charactersToMove;
-
-		// Lerp the caret
-
-	}
-	private void MoveRight(int charactersToMove = 1)
-	{
-		// Check for if we have somewhere to move to
-		if (!Maths.InRange(CaretIndex, 0, Text.Length)) return;
-		CaretIndex += charactersToMove;
-
-		// Lerp the caret
-		caretLerpEnd = 0;
-	}
-
-	// Get selected text
-	private string GetSelectedText()
-	{
-		// Check for if we actually have anything selected 
-		// in the first place (kinda important ngl)
-		if (selectingSomething == false) return null;
-
-		// Get the selected text
-		return Text.Substring(selection.StartIndex, selection.Length);
-	}
-
-	// Get current content. This can either be the current
-	// sentence, or the current line (depends on what you want)
-	// or maybe even current paragraph idk bru
-	private string GetCurrentContent()
-	{
-		//! Placeholder
-		return Text;
-	}
-
-	// Selection stuff
-	private void PrimeSelection()
-	{
-		// Make a new selection if there is not already one
-		if (selectingSomething) return;
-
-		selection = new Selection()
-		{
-			StartIndex = CaretIndex,
-			EndIndex = CaretIndex
-		};
-	}
-	private void Deselect() => selection = null;
-	private void SelectAll()
-	{
-		PrimeSelection();
-		selection.StartIndex = 0;
-		selection.EndIndex = Text.Length;
-	}
-
-	// Get stuff around the caret
-	private string GetContentBeforeCaret() => Text.Substring(0, Text.Length - CaretIndex);
-
-
-
-
-
 
 	// TODO: Don't use a class
 	private class Selection
@@ -237,6 +71,34 @@ public class TextInput : RenderableComponent
 				int selectionEnd = Math.Max(StartIndex, EndIndex);
 				return selectionEnd - selectionStart;
 			}
+		}
+	}
+
+	private class Caret
+	{
+		// TODO: Don't do this
+
+		public int Index;
+		public int Line;
+
+		private float animationDuration = 1f;
+
+		public void MoveBackwards(int charactersToMove = 1)
+		{
+			// MoveTo(position - (charactersToMove * Vector2.UnitX));
+		}
+
+		public void MoveForwards(int charactersToMove = 1)
+		{
+			// MoveTo(position + (charactersToMove * Vector2.UnitX));
+		}
+
+		// TODO: Add block caret support
+		public void Draw()
+		{
+			// string textUpUntilCaret = ContentBeforeCaret();
+			// Vector2 caretPosition = position + (MeasureText(textUpUntilCaret, FontSize) + caret.VisualPosition * Vector2.UnitX);
+			// DrawSquare(caretPosition, new Vector2(FontSize / 15, FontSize), Color.White);
 		}
 	}
 }
