c50983f9501776467326b2c85c5cd29d7b679100
MaximilianMcC <max@maximilian.co.nz>
2025-07-17 14:42:44 +1200

    Can move and backspace

--- Library/src/Components/TextInput.cs
+++ Library/src/Components/TextInput.cs
@@ -7,8 +7,10 @@ namespace Smoke;
 
 public class TextInput : RenderableComponent
 {
-	public List<string> Lines = [];
+	public List<string> Lines = [ "" ];
+
 	public string Text => string.Join("\n", Lines);
+	public string CurrentLine => Lines[mainCaret.Line];
 
 	private List<Caret> carets = [];
 	private Caret mainCaret => carets[0];
@@ -17,6 +19,13 @@ public class TextInput : RenderableComponent
 
 	public float FontSize = 32;
 	public Color SelectionColor = 0x162F58_80;
+
+	public override void Start()
+	{
+		// Add the 'main' caret
+		//! This should never be removed
+		carets.Add(new Caret(this));
+	}
 
 	public override void Update()
 	{
@@ -28,23 +37,30 @@ public class TextInput : RenderableComponent
 			for (int i = 0; i < input.Count; i++)
 			{
 				// Add it to the text
-				WriteAfterCursor(input[i].ToString());
+				WriteAfterCaret(input[i].ToString());
 			}
 		}
+
+		// Handle fancy other buttons
+		if (KeyPressedAndHeld(KeyboardKey.Backspace)) DeleteBeforeCaret();
+		if (KeyPressedAndHeld(KeyboardKey.Left)) carets.ForEach(caret => caret.MoveBackwards());
+		if (KeyPressedAndHeld(KeyboardKey.Right)) carets.ForEach(caret => caret.MoveForwards());
 	}
 
 	// Draw everything idk
 	public override void Render2D()
 	{
 		//!debug
-		Vector2 position = new Vector2(20);
+		Vector2 position = new Vector2(50);
 
 		// Draw the text normally ig
 		DrawText(Text, position, Origin.TopLeft, 0f, FontSize, Color.White);
+
+		DrawText("KIA ORA", 100, Color.Black);
 	}
 
 	// Append text where the cursor is (normal writing)
-	private void WriteAfterCursor(string newText)
+	private void WriteAfterCaret(string newText)
 	{
 		// Loop over all the carets
 		// TODO: Make a caret handler class that does the looping for me
@@ -54,6 +70,27 @@ public class TextInput : RenderableComponent
 			Lines[caret.Line] = Lines[caret.Line].Insert(caret.Index, newText);
 			caret.MoveForwards(newText.Length);
 		}
+	}
+
+	// Delete before before the cursor (normal deleting)
+	// TODO: Rename to backspace
+	private void DeleteBeforeCaret()
+	{
+		// Loop over all the carets
+		// TODO: Make a caret handler class that does the looping for me
+		foreach (Caret caret in carets)
+		{
+			// Remove the text
+			caret.MoveBackwards();
+			Lines[caret.Line] = Lines[caret.Line].Remove(caret.Index, 1);
+		}
+	}
+
+	// Delete before after the cursor (del key)
+	// TODO: Rename to delete
+	private void DeleteAfterCaret()
+	{
+
 	}
 
 	// TODO: Don't use a class
@@ -77,20 +114,53 @@ public class TextInput : RenderableComponent
 	private class Caret
 	{
 		// TODO: Don't do this
+		private TextInput textInput;
 
 		public int Index;
 		public int Line;
+
+		public Caret(TextInput parent)
+		{
+			textInput = parent;
+		}
 
 		private float animationDuration = 1f;
 
 		public void MoveBackwards(int charactersToMove = 1)
 		{
-			// MoveTo(position - (charactersToMove * Vector2.UnitX));
+			Index -= charactersToMove;
+
+			// Check for if we've gone too far backwards
+			if (Index < 0)
+			{
+				// Go to the start of the line
+				Index = 0;
+
+				// If there is a line above, then go there
+				if (Line != 0) Line--;
+			}
 		}
 
 		public void MoveForwards(int charactersToMove = 1)
 		{
-			// MoveTo(position + (charactersToMove * Vector2.UnitX));
+			Index += charactersToMove;
+
+			// Check for if we're at the end of the line
+			if (Index > textInput.CurrentLine.Length)
+			{
+				// If there is a below above, then go there,
+				// otherwise just don't let us leave this line
+				if (Line != textInput.Lines.Count - 1)
+				{
+					// Go to the start of the next line
+					Line++;
+					Index = 0;
+				}
+				else
+				{
+					Index = textInput.CurrentLine.Length;
+				}
+			}
 		}
 
 		// TODO: Add block caret support
