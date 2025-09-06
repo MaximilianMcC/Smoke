047d0f837d9dedb8e33f688d0152afc6b20a7dd0
MaximilianMcC <max@maximilian.co.nz>
2025-09-07 00:56:20 +1200

    coordinate and index converters

--- Library/src/Api/Maths.cs
+++ Library/src/Api/Maths.cs
@@ -13,4 +13,17 @@ public static class Maths
 	{
 		return (variable >= lower) && (variable <= upper);
 	}
+
+	public static Vector2 IndexToCoordinates(int index, int width)
+	{
+		return new Vector2(
+			index / width,
+			index % width
+		);
+	}
+
+	public static int CoordinatesToInddex(Vector2 coordinates, int width)
+	{
+		return (int)((coordinates.Y * width) + coordinates.X);
+	}
 }
