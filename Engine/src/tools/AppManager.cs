09dc2ce4dba9ae1f28d8827bb81123d0d9d98690
MaximilianMcC <max@maximilian.co.nz>
2025-10-29 14:13:22 +1300

    delete everything for restart

--- Engine/src/tools/AppManager.cs
+++
@@ -1,9 +0,0 @@
-class AppManager
-{
-	public static string GetRunnerPath()
-	{
-		// TODO: If smoke is not already installed then tell to install
-		//? "Smoke cannot be found on this machine. Use `smoke install` to install it."
-		return "erhm";
-	}
-}
