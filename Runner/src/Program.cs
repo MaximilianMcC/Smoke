3600dee894dd2cd48e379320244f912ad7831e56
MaximilianMcC <max@maximilian.co.nz>
2025-11-11 00:14:39 +1300

    added runners raylib window (the actual game is a window now)

--- Runner/src/Program.cs
+++ Runner/src/Program.cs
@@ -1,18 +1,30 @@
 using System.Reflection;
+using Raylib_cs;
 using Smoke;
 
 class Program
 {
 	public static void Main(string[] args)
 	{
-		// Load the smoke project from argument
+		// Load the smoke project from arguments
 		SmokeProject.Instance.Load(args[0]);
 
-		//! debug print all loaded assemblies
-		foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+		// Create the raylib window
+		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
+		Raylib.InitWindow(854, 480, SmokeProject.Instance.Settings.DisplayName);
+		Raylib.SetConfigFlags(ConfigFlags.ResizableWindow | ConfigFlags.AlwaysRunWindow);
+		Raylib.SetExitKey(KeyboardKey.Null);
+
+		// Main raylib loop
+		while (Raylib.WindowShouldClose() == false)
 		{
-			Console.WriteLine(assembly.Location);
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.Magenta);
+			Raylib.DrawText(SmokeProject.Instance.Settings.DisplayName, 10, 10, 30, Color.White);
+			Raylib.EndDrawing();
 		}
-			
+
+		// Close raylib
+		Raylib.CloseWindow();
 	}
 }
